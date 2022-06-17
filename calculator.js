1a1705ea0e47ea58fbfd8db1cc8b4d10106c8007
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 18:35:04 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,17 +1,24 @@
 let button = document.querySelectorAll('#calculator button');
+let total;
 
 for (let i in button) {
-  button[i].onclick = function() {
+  button[i].onclick = function(e) {
     if (this.innerText.indexOf('C') !== -1) {
           document.querySelector('#results').innerText = '';
         } else if (this.innerText.indexOf('<') !== -1) {
           document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
         } else if (this.innerText.indexOf('=') === -1) {
-          document.querySelector('#results').innerText += button[i].innerText;
+          if (button[i].className === 'add') {
+            total = parseInt(button[i].innerText + button[i].innerText, 10);
+          } else if (button[i].className === 'subtract') {
+            total = parseInt(button[i].innerText - button[i].innerText, 10);
+          } else if (button[i].className === 'multiply') {
+            total = parseInt(button[i].innerText * button[i].innerText, 10);
+          } else if (button[i].className === 'divide') {
+            total = parseInt(button[i].innerText / button[i].innerText, 10);
+          }
+          
+          document.querySelector('#results').innerText = total;
         }
   };
 }
-
-document.querySelector('#equals').onclick = function() {
-  document.querySelector('#results').innerText = equals;
-};
