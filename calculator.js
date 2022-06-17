6ff56d7029aee96787e3ae0be0b939de893e4b93
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 16:29:58 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -16,12 +16,11 @@ document.querySelector('#equals').onclick = function() {
     let doMath = document.querySelectorAll('#results span.doMath').innerText;
   
   for (let i in doMath) {
-if (doMath[i] === '+') {
-  doMath[i] = '+';
+if (doMath[i] === '&#43;') {
+  doMath[i].replace(/&#43;/g, '+');
 }
   }
   
   let equals = parseInt(document.querySelector('#results').innerText, 10);
-  
   document.querySelector('#results').innerText = equals;
-}
+};
