66393fcb92797b269b668631fef77ab5613c38db
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 15:29:56 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,13 @@
+let button = document.querySelectorAll('#calculator button');
 
+for (let i in button) {
+  button[i].onclick = function() {
+    if (this.innerText.indexOf('C') !== -1) {
+          button[i].innerText = '';
+        } else if (this.innerText.indexOf('<') !== -1) {
+          button[i].charAt(0).innerText = '';
+        } else {
+          document.querySelector('#results').innerText += button[i].innerText;
+        }
+  };
+}
