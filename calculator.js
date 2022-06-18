2a6d2dfdedab8e0a936cbb88febb518650326a0c
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 19:05:17 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 let button = document.querySelectorAll('#calculator button');
-let total;
+let total = 0;
 
 for (let i in button) {
   button[i].onclick = function(e) {
@@ -9,14 +9,15 @@ for (let i in button) {
           document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
         } else if (this.innerText.indexOf('=') === -1) {
           if (button[i].className === 'add') {     
-            total = parseInt(button[i].innerText + button[i].innerText, 10);
+            total = parseInt(button[i].innerText + document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1);, 10);
           } else if (button[i].className === 'subtract') {
-            total = parseInt(button[i].innerText - button[i].innerText, 10);
+            total = parseInt(button[i].innerText - document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1);, 10);
           } else if (button[i].className === 'multiply') {
-            total = parseInt(button[i].innerText * button[i].innerText, 10);
+            total = parseInt(button[i].innerText * document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1);, 10);
           } else if (button[i].className === 'divide') {
-            total = parseInt(button[i].innerText / button[i].innerText, 10);
+            total = parseInt(button[i].innerText / document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1);, 10);
           }
         }
+    console.log(total);
   };
 }
