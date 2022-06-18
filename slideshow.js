e406b1c4356d73d84c904e4e8d276ba6d335764c
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 21:48:11 -0500

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,8 @@
 let slide = document.querySelectorAll('div.slide');
 
-for (let i in slide) {
-  if(slide[i].style.display === 'block') {
+  if(slide.style.display === 'block') {
     document.querySelector('#next').onclick = function() {
-      slide[i].style.display = 'none';
-      slide[i].nextSibling.visibility = 'block';
+      slide.style.display = 'none';
+      slide.nextSibling.visibility = 'block';
     };
   }
-}
