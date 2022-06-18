877977e3b83ca735a34477bab541efcd89804e3a
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 20:22:14 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,35 +3,39 @@ let total = 0;
 let num;
 
 for (let i in button) {
-  button[i].onclick = function(e) {
-    if (this.innerText.indexOf('C') !== -1) {
-          document.querySelector('#results').innerText = '';
+    button[i].onclick = function(e) {
+        if (this.innerText.indexOf('C') !== -1) {
+            document.querySelector('#results').innerText = '';
         } else if (this.innerText.indexOf('<') !== -1) {
-          document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
+            document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
         } else if (this.className === 'num') {
-          num = this.innerText;
-          document.querySelector('#results').innerText += num;
-        } else if (this.className === 'add') {
+            num = this.innerText;
+            document.querySelector('#results').innerText += num;
+        }
+        if (this.className === 'add') {
             document.querySelector('#results').innerText += '+';
-          if (this.className === 'num') {
+        }
+        if (this.className === 'num') {
             total = parseInt(num + document.querySelector('#results').innerText.substring(document.querySelector('#results').innerText.length, document.querySelector('#results').innerText.length - 1), 10);
-          }
-          } else if (this.className === 'subtract') {
+        }
+        if (this.className === 'subtract') {
             document.querySelector('#results').innerText += '-';
-            if (this.className === 'num') {
+        }
+        if (this.className === 'num') {
             total = parseInt(num - document.querySelector('#results').innerText.substring(document.querySelector('#results').innerText.length, document.querySelector('#results').innerText.length - 1), 10);
-            }
-          } else if (this.className === 'multiply') {
+        }
+        if (this.className === 'multiply') {
             document.querySelector('#results').innerText += '*';
-            if (this.className === 'num') {
+        }
+        if (this.className === 'num') {
             total = parseInt(num * document.querySelector('#results').innerText.substring(document.querySelector('#results').innerText.length, document.querySelector('#results').innerText.length - 1), 10);
-            }
-          } else if (this.className === 'divide') {
+        }
+        if (this.className === 'divide') {
             document.querySelector('#results').innerText += '/';
             if (this.className === 'num') {
-            total = parseInt(num / document.querySelector('#results').innerText.substring(document.querySelector('#results').innerText.length, document.querySelector('#results').innerText.length - 1), 10);
+                total = parseInt(num / document.querySelector('#results').innerText.substring(document.querySelector('#results').innerText.length, document.querySelector('#results').innerText.length - 1), 10);
             }
-          }
-        };
+        }
+    };
     console.log(total);
-  }
+}
