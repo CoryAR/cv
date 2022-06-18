11fc19d91c1256259c0b47f872b03a7edb915a11
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 19:46:11 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 let button = document.querySelectorAll('#calculator button');
 let total = 0;
+let num;
 
 for (let i in button) {
   button[i].onclick = function(e) {
@@ -8,7 +9,7 @@ for (let i in button) {
         } else if (this.innerText.indexOf('<') !== -1) {
           document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
         } else if (this.className === 'num') {
-          let num = this.innerText;
+          num = this.innerText;
           document.querySelector('#results').innerText += num;
         } else if (this.className === 'add') {
             document.querySelector('#results').innerText += '+';
@@ -23,7 +24,6 @@ for (let i in button) {
             document.querySelector('#results').innerText += '/';
             total = parseInt(num / document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
           }
-        }
+        };
     console.log(total);
-  };
-}
+  }
