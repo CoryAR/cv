990e5b65d9416c36765009bf2c2035f3c9197bda
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-17 19:18:40 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,15 +7,16 @@ for (let i in button) {
           document.querySelector('#results').innerText = '';
         } else if (this.innerText.indexOf('<') !== -1) {
           document.querySelector('#results').innerText = document.querySelector('#results').innerText.slice(0, -1);
-        } else if (this.innerText.indexOf('=') === -1) {
+        } else if (this.className === 'num') {
+          let num = button[i].innerText;
           if (button[i].className === 'add') {     
-            total = parseInt(button[i].innerText + document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
+            total = parseInt(num + document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
           } else if (button[i].className === 'subtract') {
-            total = parseInt(button[i].innerText - document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
+            total = parseInt(num - document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
           } else if (button[i].className === 'multiply') {
-            total = parseInt(button[i].innerText * document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
+            total = parseInt(num * document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
           } else if (button[i].className === 'divide') {
-            total = parseInt(button[i].innerText / document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
+            total = parseInt(num / document.querySelector('#results').innerText.substring(5, document.querySelector('#results').innerText.length - 1), 10);
           }
         }
     console.log(total);
