5228d0f58be84effa1a1213249633da4ca373af1
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-18 21:02:01 -0500

    Update pop_outs.js

--- pop_outs.js
+++ pop_outs.js
@@ -1,13 +1,13 @@
 let popOut = document.querySelectorAll('#pop-outs a');
 
-    let removePopouts = document.querySelectorAll('div.popouts');
-    
-    for (let x in removePopouts) {
-      removePopouts[x].parentNode.removeChild(removePopouts[x]);
-    }
-
 for (let i in popOut) {
   popOut[i].onclick = function() {
+    let removePopouts = document.querySelectorAll('div.popouts');
+    
+    while(removePopouts.length > 0){
+      removePopouts[0].parentNode.removeChild(removePopouts[0]);
+    }
+      
     let pos = popOut[i].getBoundingClientRect();
     let div = document.createElement('div');
     div.className = 'popouts';
