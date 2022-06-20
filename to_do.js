d84659b807e007b039bb678ec6e71edb86cf8177
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-06-19 19:38:29 -0500

    Update to_do.js

--- to_do.js
+++ to_do.js
@@ -10,8 +10,6 @@ function removeItem() {
 
         if (localStorage.getItem('remove-to-do-' + remove_to_do[i].previousElementSibling.innerText.trim()) !== null && localStorage.getItem('remove-to-do-' + remove_to_do[i].previousElementSibling.innerText.trim()) === 'true') {
             remove_to_do[i].parentNode.style.display = 'none';
-        } else if (localStorage.getItem('remove-to-do-' + remove_to_do[i].previousElementSibling.innerText.trim()) !== 'true') {
-            remove_to_do[0].parentNode.style.display = 'block';
         }
     }
 }
