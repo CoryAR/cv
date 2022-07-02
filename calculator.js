a13cc2e577b21fe5630c4ec489e6801d19e34182
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-02 18:08:10 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,11 @@
 let calc = document.querySelectorAll('#calculator input.btn');
 
+let results = {
+  firstNumber: firstNumber,
+  secondNumber: secondNumber,
+  operator: operator
+};
+
 for (let i in calc) {
 calc[i].addEventListener('click', function () {
   if (this.value !== '=' && this.value !== 'C') {
@@ -7,7 +13,10 @@ calc[i].addEventListener('click', function () {
   } else if (this.value === 'C') {
     document.querySelector('input[name="results"]').value = '';
   } else if (this.value === '=') {
-    
+    results.operator = document.querySelector('input[name="results"]').value.match(/\+|-|\*|\//i);
+    results.firstNumber = document.querySelector('input[name="results"]').value.match(/[0-9]+/i)[0];
+    results.secondNumber = document.querySelector('input[name="results"]').value.match(/[0-9]+/i)[1];
+    console.log(results);
   }
 });
 }
