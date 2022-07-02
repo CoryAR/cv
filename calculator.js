123e0eaa3a1f6a0ca2accce0eae016125eb23a4f
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-01 22:23:19 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ let calc = document.querySelectorAll('#calculator input.btn');
 
 for (let i in calc) {
 calc[i].addEventListener('click', function () {
-  if (this.value !== '=') {
+  if (this.value !== '=' || this.value !== 'C') {
   document.querySelector('input[name="results"]').value += this.value;
   } else if (this.value === 'C') {
     document.querySelector('input[name="results"]').value = '';
