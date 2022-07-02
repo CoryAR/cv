9b16ed81fc9e6d480ca104398470beb62c3d5be9
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-02 00:28:03 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -13,7 +13,8 @@ calc[i].addEventListener('click', function () {
     
   for (let i in calculations) {
       if (calculations[i].indexOf('+') !== -1) {
-      document.querySelector('input[name="results"]').value = parseInt(calculations[(i+len-1)%len] calculations[i] calculations[(i+1)%len], 10);
+      console.log(calculations[(i+len-1)%len])
+        console.log(calculations[(i+1)%len]));
     } else if (calculations[i].indexOf('-') !== -1) {
       document.querySelector('input[name="results"]').value = parseInt(calculations[i - 1] - calculations[i + 1], 10);
     } else if (calculations[i].indexOf('*') !== -1) {
