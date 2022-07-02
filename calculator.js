ba526051136e607f34ab66772f0216ef17b86d6b
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-01 23:52:39 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -9,21 +9,18 @@ calc[i].addEventListener('click', function () {
     document.querySelector('input[name="results"]').value = '';
   } else if (this.value === '=') {
       calculations.push(document.querySelector('input[name="results"]').value.match(/[0-9]+|\+|-|\*|\//g));
-  let value = 0;
     
   for (let i in calculations) {
       if (calculations[i] === '+') {
-      value = parseInt(calculations[i - 1] += calculations[i + 1], 10);
+      document.querySelector('input[name="results"]').value = parseInt(calculations[i - 1] += calculations[i + 1], 10);
     } else if (calculations[i] === '-') {
-      value = parseInt(calculations[i - 1] -= calculations[i + 1], 10);
+      document.querySelector('input[name="results"]').value = parseInt(calculations[i - 1] -= calculations[i + 1], 10);
     } else if (calculations[i - 1] === '*') {
-      value = parseInt(calculations[i - 1] *= calculations[i + 1], 10);
+      document.querySelector('input[name="results"]').value = parseInt(calculations[i - 1] *= calculations[i + 1], 10);
     } else if (calculations[i] === '/') {
-      value = parseInt(calculations[i - 1] /= calculations[i + 1], 10);
+      document.querySelector('input[name="results"]').value = parseInt(calculations[i - 1] /= calculations[i + 1], 10);
     }
-    return calculations[i + 1];
   }
-    document.querySelector('input[name="results"]').value = value;
   }
 });
 }
