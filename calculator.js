39bafd1d128808412b10111e6d5cbb9c13c8cce8
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-02 19:23:54 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,33 +1,34 @@
 let calc = document.querySelectorAll('#calculator input.btn');
+let results = document.querySelector('input[name="results"]');
 
-let results = {
-  firstNumber: 0,
-  secondNumber: 0
+let numbers = {
+    firstNumber: 0,
+    secondNumber: 0
 };
 
 for (let i in calc) {
-calc[i].addEventListener('click', function () {
-  if (this.value !== '=' && this.value !== 'C') {
-  document.querySelector('input[name="results"]').value += this.value;
-    
-    if (document.querySelector('input[name="results"]').value.length === 3 && document.querySelector('input[name="results"]').value.match(/\+|-|\*|\//i)) {
-      document.querySelector('#calculator input.equals').click();
-    }
-  } else if (this.value === 'C') {
-    document.querySelector('input[name="results"]').value = '';
-  } else if (this.value === '=') {
-    results.firstNumber = parseInt(document.querySelector('input[name="results"]').value.match(/[0-9]+/i)[0], 10);
-    results.secondNumber = parseInt(document.querySelector('input[name="results"]').value.split(/\+|-|\*|\//i)[1], 10);
-    
-    if (document.querySelector('input[name="results"]').value.includes('+')) {
-    document.querySelector('input[name="results"]').value = results.firstNumber + results.secondNumber;
-  } else if (document.querySelector('input[name="results"]').value.includes('-')) {
-    document.querySelector('input[name="results"]').value = results.firstNumber - results.secondNumber;
-  } else if (document.querySelector('input[name="results"]').value.includes('*')) {
-    document.querySelector('input[name="results"]').value = results.firstNumber * results.secondNumber;
-  } else if (document.querySelector('input[name="results"]').value.includes('/')) {
-    document.querySelector('input[name="results"]').value = results.firstNumber / results.secondNumber;
-  }
-  }
-});
+    calc[i].addEventListener('click', function() {
+        if (this.value !== '=' && this.value !== 'C') {
+            results.value += this.value;
+
+            if (results.value.length === 3 && results.value.match(/\+|-|\*|\//i)) {
+                document.querySelector('#calculator input.equals').click();
+            }
+        } else if (this.value === 'C') {
+            results.value = '';
+        } else if (this.value === '=') {
+            numbers.firstNumber = parseInt(results.value.match(/[0-9]+/i)[0], 10);
+            numbers.secondNumber = parseInt(results.value.split(/\+|-|\*|\//i)[1], 10);
+
+            if (results.value.includes('+')) {
+                results.value = numbers.firstNumber + numbers.secondNumber;
+            } else if (results.value.includes('-')) {
+                results.value = numbers.firstNumber - numbers.secondNumber;
+            } else if (results.value.includes('*')) {
+                results.value = numbers.firstNumber * numbers.secondNumber;
+            } else if (results.value.includes('/')) {
+                results.value = numbers.firstNumber / numbers.secondNumber;
+            }
+        }
+    });
 }
