680d3e626e894a615598e3000d9c66d9f3591110
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-02 19:10:03 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -9,15 +9,15 @@ for (let i in calc) {
 calc[i].addEventListener('click', function () {
   if (this.value !== '=' && this.value !== 'C') {
   document.querySelector('input[name="results"]').value += this.value;
+    
+    if (document.querySelector('input[name="results"]').value.length === 3) {
+      document.querySelector('#calculator input.equals').click();
+    }
   } else if (this.value === 'C') {
     document.querySelector('input[name="results"]').value = '';
   } else if (this.value === '=') {
     results.firstNumber = parseInt(document.querySelector('input[name="results"]').value.match(/[0-9]+/i)[0], 10);
     results.secondNumber = parseInt(document.querySelector('input[name="results"]').value.split(/\+|-|\*|\//i)[1], 10);
-    
-    if (document.querySelector('input[name="results"]').value.length === 3) {
-      document.querySelector('#calculator input.equals')[0].click();
-    }
     
     if (document.querySelector('input[name="results"]').value.includes('+')) {
     document.querySelector('input[name="results"]').value = results.firstNumber + results.secondNumber;
