367a74b3c86c98932a21c4db119408362bc871da
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-06 21:35:29 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -2,9 +2,9 @@ function daysInMonth(month, year) {
   return new Date(year, month, 0).getDate();
 }
 
-let monthDays = daysInMonth(new Date().getMonth(), new Date().getFullYear());
-let startDate = 0;
+let monthDays = [];
 
 for (let i = 0; i < monthDays; i++) {
-document.querySelector('#calendar').append(startDate[i]++);
+  monthDays[i].push(daysInMonth(new Date().getMonth(), new Date().getFullYear()));
+  document.querySelector('#calendar').append(monthDays[i]);
 }
