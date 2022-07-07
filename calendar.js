63b7e2c4f383ed3c4f2f3c6e9ac914a2a578f7e0
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-06 21:11:28 -0500

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,10 @@
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+let monthDays = daysInMonth(new Date().getMonth(), new Date().getFullYear())
+let startDate = 0;
+
+for (let i in monthDays) {
+documentQuerySelector('#calendar').appendChild(startDate[i]++);
+}
