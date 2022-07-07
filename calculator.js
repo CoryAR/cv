8309245d17ef3d0c0e3ad2aa14df881d53360c4d
CoryAR <83148797+CoryAR@users.noreply.github.com>
2022-07-06 20:23:53 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,20 +11,20 @@ for (let i in calc) {
         if (this.value !== '=' && this.value !== 'C') {
             results.value += this.value;
 
-            if (results.value.match(/[0-9\.]+/g).length >= 2 && results.value.match(/\+|−|X|\//i).length === 1) {
+            if (results.value.match(/[0-9\.]+/g).length >= 2 && results.value.match(/\+|−|x|\//i).length === 1) {
                 document.querySelector('#calculator input.equals').click();
             }
         } else if (this.value === 'C') {
             results.value = '';
         } else if (this.value === '=') {
             numbers.firstNumber = parseFloat(results.value.match(/[0-9]+/i)[0], 10);
-            numbers.secondNumber = parseFloat(results.value.split(/\+|−|X|\//i)[1], 10);
+            numbers.secondNumber = parseFloat(results.value.split(/\+|−|x|\//i)[1], 10);
 
              if (results.value.includes('+')) {
                 results.value = numbers.firstNumber + numbers.secondNumber;
             } else if (results.value.includes('−')) {
                 results.value = numbers.firstNumber - numbers.secondNumber;
-            } else if (results.value.includes('X')) {
+            } else if (results.value.includes('x')) {
                 results.value = numbers.firstNumber * numbers.secondNumber;
             } else if (results.value.includes('/')) {
                 results.value = numbers.firstNumber / numbers.secondNumber;
